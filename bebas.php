a9d731cf51e588c2b23aec818446361d6087d849
KostsPlayer <azka.nuril070@gmail.com>
2022-12-15 10:54:44 +0700

    Modul VIII

---
+++ bebas.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis modi ipsum nam vero, ullam minima debitis odit commodi sunt similique cumque.</p>
+
+    <!-- PHP -->
+
+    <?php
+        $txt = "PHP";
+        echo "I love $txt!";
+    ?>  <br>
+
+    <?php
+    echo "Hello World!";
+    ?> <br>
+
+    <?php
+        $color = "red";
+        echo "My car is " . $color . "<br>";
+        echo "My house is " . $COLOR . "<br>";
+        echo "My boat is " . $coLOR . "<br>";
+    ?> <br>
+
+    <?php
+        echo "<h2>PHP is Fun!</h2>";
+        echo "Hello world!<br>";
+        echo "I'm about to learn PHP!<br>";
+        echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+    ?>
+
+</body>
+</html>
---
+++ while.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Penggunaan Kontrol While</title>
+</head>
+<body>
+    <?php
+        $hari=array("Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu","Minggu");
+        $i=0;
+        while($i<count($hari))
+        {
+            echo ("Hari ke " .($i+1). " adalah " .$hari[$i]. "<br>");
+            $i++;
+        }
+    ?>
+</body>
+</html>
